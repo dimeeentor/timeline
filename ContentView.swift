cafa08682d91d2df08a43a9a87cf3ed97f4baba9
Dmytro Honcharenko <dimeeentor@gmail.com>
2024-03-12 22:29:18 +0100

    small impovements (design)
    1. Fixed styles of main view to have good looking on both light and dark mode.
    2. Implemented custom sketch sheet design.

--- ContentView.swift
+++ ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var content = []
+    @State private var isSheetPresenting = false
 
     var body: some View {
         NavigationStack {
@@ -25,18 +25,44 @@ struct ContentView: View {
                     }
                     .padding(24)
                 }
-
-                Button(action: {}) {
-                    Text("New event...")
-                        .padding()
-                        .foregroundStyle(.black.opacity(0.85))
-                        .backgroundStyle(.red)
-                        
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             }
             .navigationTitle("Timeline")
             .scrollIndicators(.never)
+            .sheet(isPresented: $isSheetPresenting) {
+                // TODO: Implement logic
+                Spacer()
+                Button(action: {
+                    isSheetPresenting = false
+                }) {
+                    Text("Done")
+                        .padding()
+                        .foregroundStyle(.pink)
+                        .frame(maxWidth: .infinity)
+                        .background(.white.secondary)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 18)
+                                .stroke(style: .init(lineWidth: 1, dash: [10])
+                                )
+                                .foregroundStyle(.pink)
+                        }
+                        .clipShape(.rect(cornerRadius: 18))
+                        .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+                }
+                .presentationDetents([.medium])
+                .presentationBackground(alignment: .center) {
+                    RoundedRectangle(cornerRadius: 36)
+                        .foregroundStyle(.white)
+                        .padding()
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: { isSheetPresenting = true }) {
+                        Image(systemName: "plus")
+                            .tint(.pink)
+                    }
+                }
+            }
         }
     }
 }
@@ -45,11 +71,13 @@ struct ContentView: View {
     ContentView()
 }
 
+// TODO: Separate into simpler view
 struct TimelineCellView: View {
     let cellHeight: CGFloat = 200
-    
+
     var body: some View {
         VStack(alignment: .leading) {
+            // Decoration
             Rectangle()
                 .foregroundStyle(.gray.quinary)
                 .frame(width: cellHeight + 20, height: 5)
@@ -60,14 +88,21 @@ struct TimelineCellView: View {
                 )
 
             HStack(spacing: 1) {
+                // Decoration
                 Rectangle()
                     .foregroundStyle(.gray.quinary)
                     .frame(width: 20, height: 5)
 
+                // Cell
                 RoundedRectangle(cornerRadius: 18.0)
                     .foregroundStyle(.regularMaterial)
                     .frame(width: .infinity, height: cellHeight)
                     .shadow(radius: 2)
+                    .contentShape(.contextMenuPreview, RoundedRectangle(cornerRadius: 18.0))
+                    .contextMenu(ContextMenu(menuItems: {
+                        Text("Edit...")
+                        Text("Delete")
+                    }))
             }
         }
     }
